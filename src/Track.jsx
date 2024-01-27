4c60b695f58378c1929214d040f0519e897085aa
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-27 21:59:16 +0100

    updated track section

--- src/Track.jsx
+++ src/Track.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./track.css";
 
 const Track = () => {
   return (
     <>
     <div>
       <h3>Plan and Track your budgets with NextWallet</h3>
+    </div>
+    <div className='img-wrapper'>
+        <img src="https://s3.amazonaws.com/cdns3.nerdwallet.com/img/landing/2021/app-hub/net-worth.png" alt="" srcset="" />
+        <img src="https://s3.amazonaws.com/cdns3.nerdwallet.com/img/landing/2021/app-hub/cash-flow.png" alt="" srcset="" />
+        <img src="https://s3.amazonaws.com/cdns3.nerdwallet.com/img/landing/2021/app-hub/credit-score.png" alt="" srcset="" />
     </div>
     </>
   )
