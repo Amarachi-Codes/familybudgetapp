98b89b8626cfc2a95d5a5552c3e07fb71ab5da46
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-29 14:06:46 +0100

    edited

--- src/Security.jsx
+++ src/Security.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import "./security.css";
 
 const Security = () => {
     return (
-        <div>
+        <div className='bg1 security-container'>
             <h5>Your security. Our priority.</h5>
             <p>We always have your security in mind. Rest easy knowing your data is encrypted for enhanced protection.</p>
             <p><a href="">Learn more</a></p>
