68313f003fec340943efb27bdd405f46b970e744
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-29 13:11:41 +0100

    updated

--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -5,9 +5,24 @@ const Testimonial = () => {
         <div>
             <h2 className='headind-style'>What people are saying about NextWallet</h2>
             <div>
-            <p>“Simple yet secure setup, easy to install, important info to know is shared along with helpful tips!”</p>
-            <h6>Jon S.</h6>
-            <p>Uses NextWallet app for Android</p>
+
+                <div>
+                    <p>“Simple yet secure setup, easy to install, important info to know is shared along with helpful tips!”</p>
+                    <h6>Jon S.</h6>
+                    <p>Uses NextWallet app for Android</p>
+                </div>
+
+                <div>
+                    <p>“I've been looking for an app to help me manage my finances, and although I had some good experiences with other apps, this is the one I've been looking for. It has all the features and functions to help you monitor your credit, accounts, and investments.”</p>
+                    <h6>Cameron A.</h6>
+                    <p>Uses NextWallet app for Android</p>
+                </div>
+
+                <div>
+                    <p>“I am always learning something new which I can apply....and it makes me feel empowered.”</p>
+                    <h6>Kari H.</h6>
+                    <p>Uses NextWallet app for iOS</p>
+                </div>
             </div>
         </div>
     )
