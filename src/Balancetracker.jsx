3ecf065668203ad7bb4462af860f0a8e0caad671
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-02-01 20:38:56 +0100

    updated

--- src/Balancetracker.jsx
+++ src/Balancetracker.jsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./testimonial.css";
 import "./expensetracker.css";
 
 const Balancetracker = () => {
+    const incRef = useRef("");
+    const expRef = useRef("");
+    const balRef = useRef("");
+    
+    
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const incomeValue = incRef.current.value;
+        const expenseValue = expRef.current.value;
+        const balValue = balRef.current.value; 
+        let item =[];
+        item.push(incomeValue);
+        item.push(expenseValue);
+        item.push(balValue);
+        let items = JSON.parse(localStorage.getItem("items")) ||[];
+        items.push(item)
+        localStorage.setItem("Items", JSON.stringify(items))
+
+
+        console.log("Income: "+incomeValue);
+        console.log("Expenses: "+expenseValue);
+        console.log("Balance: "+balValue);
+        console.log(item);
+
+
+    }
+
     return (
         <>
+        <form onSubmit={handleSubmit}>
             <div className='bg1 expense-container'>
                 <h4>Balance Tracker</h4>
                 <p>Track your Balance with NextWallet</p>
 
                 <div className='expense-wrapper'>
                     <label htmlFor="">Income</label>
-                    <input type="text" name="" id="" value={500000} />
+                    <input type="text" name="" id="" ref={incRef} />
                 </div>
                 <div className='expense-wrapper'>
                     <label htmlFor="">Expenses</label>
-                    <input type="text" name="" id="" value={200000} />
+                    <input type="text" name="" id="" ref={expRef} />
                 </div>
                 <div className='expense-wrapper'>
                     <label htmlFor="">Balance</label>
-                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" ref={balRef}/>
+                </div>
+                <div className='expense-wrapper'>
+                    <button className='button-1'>Submit</button>
                 </div>
             </div>
-
+            </form>
         </>
     )
 }
--- src/Expensetracker.jsx
+++ src/Expensetracker.jsx
@@ -1,25 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./expensetracker.css";
 
 const Expensetracker = (props) => {
+
+const [date, setDate]=useState("");
+const [expenses, setExpenses] =  useState("");
+const [price, setPrice] = useState("");
+ const handleDateChange = (e) =>{
+    setDate(e.target.value)
+ }
+ const handleExpensesChange = (e) =>{
+    setExpenses(e.target.value)
+ }
+ const handlePriceChange = (e) =>{
+    setPrice(e.target.value)
+ }
+ const handleFormSubmit = (e)=>{
+    e.preventDefault()
+    let itemValue = [];
+    itemValue.push(date)
+    itemValue.push(expenses)
+    itemValue.push(price)
+    let itemValues= JSON.parse(localStorage.getItem("itemValues")) || [];
+    itemValues.push(itemValue);
+    localStorage.setItem("itemValues", JSON.stringify(itemValues))
+
+    console.log("Date: "+ date);
+    console.log("Expenses "+ expenses);
+    console.log("price "+ price);
+    console.log(itemValue);
+ }
+
+ function Displayitems(){
+    let mylist = document.getElementById("mylist");
+    mylist= "";
+    let itemValues= JSON.parse(localStorage.getItem("itemValues")) || [];
+
+    itemValues.forEach((itemValue) => {
+        const divtags = document.createElement("div");
+        divtags.classList.add("myitems");
+
+        divtags.innerHTML = `
+        <p>${itemValue.date}</p>
+        <p>${itemValue.expenses}</p>
+        <p>${itemValue.price}</p>
+        `;
+        //  mylist.appendChild(divtags);
+    });
+ }
+ 
+
+
     return (
         <>
+        <form onSubmit={handleFormSubmit}>
             <div className='bg-6 expense-container'>
             <h4>{props.title}</h4>
             <p>{props.description}</p>
                 <div className='expense-wrapper'>
                    <label htmlFor="">Date</label><span className='space'></span> 
-                    <input type="date" name="" id="" value={500000} />
+                    <input type="date" name="" id="" onChange={handleDateChange} />
                 </div>
                 <div className='expense-wrapper'>
                     <label htmlFor="">{props.label}</label>
-                    <input type="text" name="" id="" placeholder={props.placeholder} /><span><input type="number" name="" className='Amount' placeholder="Price" id="" /></span>
+                    <input type="text" name="" id="" placeholder={props.placeholder} onChange={handleExpensesChange} /><span><input type="number" name="" className='Amount' placeholder="Price" id="" onChange={handlePriceChange}/></span>
                 </div>
                 <div className='expense-wrapper'>
                     <button className='button-1'>Submit</button>
                 </div>
 
             </div>
+            </form>
+            <div className='mylist'>
+                <Displayitems/>
+                </div>
         </>
     )
 }
